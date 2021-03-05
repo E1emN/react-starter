55cea0aa5ad5a81b1f06aeed70f4005c67e4b91a
Nurdan Elemanov <e1eman1337@gmail.com>
2021-03-05 15:35:52 +0600

    made api sercive

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './header.scss';
+import { api } from '../../services/api';
 
 interface HeaderPropTypes {
     greeting: string,
@@ -10,6 +11,19 @@ export const Header: React.FC<HeaderPropTypes> = (props) => {
 
     const { greeting, cool } = props;
 
+    const getData = async () => {
+        try {
+            const responce = await api.GET('todos');
+            console.log(responce)
+        } catch (e) {
+
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+ 
     return(
         <header>
             {greeting} {cool}
